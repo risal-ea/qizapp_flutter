d7d45f62caa6fde921f2e6808cf8704998c196ea
Risal <risal@Risals-MacBook-Pro.local>
2024-07-15 22:28:39 +0530

    update 1.3

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:qizzapp/question.dart';
-import 'question.dart';
+import 'quiz_brain.dart';
+
+QuizBrain quizBrain = QuizBrain();
 
 void main() => runApp(Quizzler());
 
@@ -31,14 +32,6 @@ class _QuizPageState extends State<QuizPage> {
 
   List<Widget> scoreKeeper = [];
 
-  List<Question> questionBank = [
-    Question(questionText: 'You can lead a cow down stairs but not up stairs.', questionAnswer: false),
-    Question(questionText: 'Approximately one quarter of human bones are in the feet.', questionAnswer: true),
-    Question(questionText: 'A slug\'s blood is green.', questionAnswer: true)
-  ];
-
-  int currentQuestionIndex = 0;
-
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -51,7 +44,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questionBank[currentQuestionIndex].questionText,
+                quizBrain.getQuestionText(),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -78,7 +71,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                bool correctAnswer = questionBank[currentQuestionIndex].questionAnswer;
+                bool correctAnswer = quizBrain.getQuestionAnswer();
 
                 if (correctAnswer == true){
                   scoreKeeper.add(
@@ -97,7 +90,7 @@ class _QuizPageState extends State<QuizPage> {
                 }
 
                 setState(() {
-                  currentQuestionIndex++;
+                  quizBrain.nextQuestion();
                 });
               },
             ),
@@ -119,7 +112,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
 
-                bool correctAnswer = questionBank[currentQuestionIndex].questionAnswer;
+                bool correctAnswer = quizBrain.getQuestionAnswer();
 
                 if (correctAnswer == false){
                   scoreKeeper.add(
@@ -137,7 +130,7 @@ class _QuizPageState extends State<QuizPage> {
                   );
                 }
                 setState(() {
-                  currentQuestionIndex++;
+                  quizBrain.nextQuestion();
                 });
               },
             ),
---
+++ lib/quiz_brain.dart
@@ -0,0 +1,39 @@
+import 'question.dart';
+
+class QuizBrain {
+
+  int _questionNumber = 0;
+
+  final List<Question> _questionBank = [
+    Question(questionText: 'You can lead a cow down stairs but not up stairs.', questionAnswer: false),
+    Question(questionText: 'Approximately one quarter of human bones are in the feet.', questionAnswer: true),
+    Question(questionText: 'A slug\'s blood is green.', questionAnswer: true),
+    Question(questionText: 'The capital of Australia is Sydney.', questionAnswer: false),
+    Question(questionText: 'There are 365 days in a year.', questionAnswer: true),
+    Question(questionText: 'Water boils at 90 degrees Celsius.', questionAnswer: false),
+    Question(questionText: 'The human heart has four chambers.', questionAnswer: true),
+    Question(questionText: 'Lightning never strikes the same place twice.', questionAnswer: false),
+    Question(questionText: 'The tallest building in the world is located in Dubai.', questionAnswer: true),
+    Question(questionText: 'The Great Wall of China is visible from space.', questionAnswer: false),
+    Question(questionText: 'The sun is a star.', questionAnswer: true),
+    Question(questionText: 'The Amazon River is the longest river in the world.', questionAnswer: false),
+    Question(questionText: 'Mount Everest is the highest mountain in the world.', questionAnswer: true),
+    Question(questionText: 'Venus is the closest planet to the sun.', questionAnswer: false),
+    Question(questionText: 'An octopus has three hearts.', questionAnswer: true),
+  ];
+
+  void nextQuestion(){
+    if(_questionNumber < _questionBank.length - 1){
+      _questionNumber++;
+    }
+  }
+
+  String getQuestionText(){
+    return _questionBank[_questionNumber].questionText;
+  }
+
+  bool getQuestionAnswer(){
+    return _questionBank[_questionNumber].questionAnswer;
+  }
+
+}
